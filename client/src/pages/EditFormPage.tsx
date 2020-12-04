388e1b7a7c762ae0f84d33edfff035be663d780e
Ujjwal Singhal <ujjwals@gmail.com>
2020-12-04 13:46:17 +0530

    Add edit form title to front end (experimental)

--- client/src/pages/EditFormPage.tsx
+++ client/src/pages/EditFormPage.tsx
@@ -1,6 +1,16 @@
 import React, { useState, useEffect, useRef } from "react";
-import QuestionList from "../components/QuestionList";
 import { Link, useParams } from "react-router-dom";
+
+import QuestionList from "../components/QuestionList";
+
+import useFormState from "../hooks/useFormState";
+
+//TODO:
+////MAKE UPDATE FORM ROUTE
+//ADD THE FORM TITLE CHANGE EFFECT
+//CHANGES THE DELETE FORM ROUTE TO DELETE THOSE QUESTION CORRESPOINDING TO FORM
+//CHANGE THE DELETE FORM THING TO CALL USE EFFECT INSTEAD OF REFRESHING
+//ERROR HANDLING IF YOU FEEL LIKE IT
 
 const EditFormPage: React.FC = () => {
   console.log("EDIT FORM PAGE RERENDERS");
@@ -8,6 +18,8 @@ const EditFormPage: React.FC = () => {
 
   const [form, setForm] = useState<any>();
   const [questions, setQuestions] = useState<any[]>();
+
+  const [title, handleTitle, resetTitle] = useFormState("");
 
   useEffect(() => {
     fetch(`http://localhost:7000/api/getform/${formid}`)
@@ -34,10 +46,40 @@ const EditFormPage: React.FC = () => {
     }
 
     console.log(questions);
+  }, []);
+
+  useEffect(() => {
+    fetch("http://localhost:7000/api/updateform", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(form),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   }, [form]);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setForm({ ...form, title: title });
+  };
 
   return form ? (
     <div>
+      {/*MAKE THIS FORM TOGGLABLE ON CLICK OF THE TITLE H1 */}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={title ? title : form.title}
+          onChange={handleTitle}
+        ></input>
+      </form>
       <h1>{form.title}</h1>
       <h2>{form.color_theme}</h2>
       <Link to="/">
