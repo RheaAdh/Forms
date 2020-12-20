11439936d7ad065d81e3f0c1ebfd2795128b8c16
Swapnodip Chakrabarti <swapnodip641@gmail.com>
2020-12-20 20:23:21 +0530

    added ui for more question types

--- client/src/components/Question.tsx
+++ client/src/components/Question.tsx
@@ -2,18 +2,37 @@ import React, {useState} from "react"
 
 const Question = () => {
     const [type, setType] = useState<any>(0);
+    const [options, setOptions] = useState([0])
+    const addOption = () => {
+        setOptions(() => [...options, options.length]);
+      };
     const types=[
         <div><b>Short answer text</b></div>,
         <div><b>Paragraph text</b></div>,
-        <div><b>Multiple choice #will add later#</b></div>,
-        <div><b>Checkbox #will add later#</b></div>,
-        <div><b>Dropdown #will add later#</b></div>,
+        <div><b>Multiple choice</b>
+            <ol>
+            {options.map((a)=>(<li key={a}><input type="text" /></li>))}
+            </ol>
+            <button onClick={addOption}>Add option</button>
+        </div>,
+        <div><b>Checkbox</b>
+            <ol>
+            {options.map((a)=>(<li key={a}><input type="text" /></li>))}
+            </ol>
+            <button onClick={addOption}>Add option</button>
+        </div>,
+        <div><b>Dropdown</b>
+            <ol>
+            {options.map((a)=>(<li key={a}><input type="text" /></li>))}
+            </ol>
+            <button onClick={addOption}>Add option</button>
+        </div>,
         <div><b>File upload #will add later#</b></div>,
         <div><b>Linear scale #will add later#</b></div>,
         <div><b>Multiple choice grid #will add later#</b></div>,
         <div><b>Checkbox grid #will add later#</b></div>,
-        <div><b>Date #will add later#</b></div>,
-        <div><b>Time #will add later#</b></div>,
+        <div><b>Date [dd:mm:yy]</b></div>,
+        <div><b>Time [hh:mm]</b></div>,
     ]
     return(
         <div className="question-component" >
@@ -27,7 +46,6 @@ const Question = () => {
                     const selectedType = e.target.value;
                     setType(selectedType);
                 }}>
-                    {/*Selection only work by click */}
                     <option value={0} >Short text</option>
                     <option value={1} >Paragraph</option>
                     <option value={2} >Multiple choice</option>
