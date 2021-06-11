44aedfe18ab6382776e1832800b1e5a771dcc888
RheaAdh <rheadhikari@gmail.com>
2021-06-11 12:51:59 +0530

    check example.env have put details

--- api/src/config/mongo.ts
+++ api/src/config/mongo.ts
@@ -2,25 +2,26 @@ import mongoose, { Collection } from "mongoose"
 const session = require("express-session")
 const MongoDBSession = require("connect-mongodb-session")(session)
 
-export function connectMongo() {
+export const connectMongo = async () => {
     console.log("Haha")
-    var mongouri: string = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@localhost:27017/forms`
+    var mongouri: string = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1zzhu.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
     console.log("haha")
-    mongoose.connect(
+    const con = await mongoose.connect(
         mongouri,
         {
             useNewUrlParser: true,
-            useUnifiedTopology: true,
             useCreateIndex: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true,
         },
         (err: any) => {
             if (err) console.log(err)
-            else console.log("Connected to DB!!!")
+            else console.log(`Connected to DB ! `)
         }
     )
 }
 
 export const store = new MongoDBSession({
-    uri: `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@localhost:27017/forms`,
+    uri: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1zzhu.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
     collection: "AllSessions",
 })
