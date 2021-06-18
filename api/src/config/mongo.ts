25563dffe08a0b19afd84865d22ba29ca8cb01b0
Abhijeet Sinha <abhijeetaman007@gmail.com>
2021-06-18 22:51:41 +0530

    Template cant be changed

--- api/src/config/mongo.ts
+++ api/src/config/mongo.ts
@@ -5,6 +5,8 @@ const MongoDBSession = require("connect-mongodb-session")(session)
 export const connectMongo = async () => {
     console.log("Haha")
     var mongouri: string = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1zzhu.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+    // var mongouri: string = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@localhost:27017/forms`
+    
     console.log("haha")
     const con = await mongoose.connect(
         mongouri,
@@ -23,5 +25,6 @@ export const connectMongo = async () => {
 
 export const store = new MongoDBSession({
     uri: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1zzhu.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+    // uri: `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@localhost:27017/forms`,
     collection: "AllSessions",
 })
--- api/src/routes/question.ts
+++ api/src/routes/question.ts
@@ -46,98 +46,105 @@ export async function addQuestion(req: Request, res: Response) {
             required: required,
         }
 
-        let newQuestion
+        console.log("Inside add ques")
+        console.log("isTemplate is " + form.isTemplate)
+        if (form.isTemplate) {
+            console.log("Template cant be editted")
+            return res.send({ success: false, msg: "Template cant be editted" })
+        } else {
+            let newQuestion
 
-        switch (question_type) {
-            case "short-answer": {
-                newQuestion = new shortQuestion({ ...common })
-                break
+            switch (question_type) {
+                case "short-answer": {
+                    newQuestion = new shortQuestion({ ...common })
+                    break
+                }
+
+                case "email-answer": {
+                    newQuestion = new emailQuestion({ ...common })
+                    break
+                }
+
+                case "paragraph-answer": {
+                    newQuestion = new paragraphQuestion({ ...common })
+                    break
+                }
+
+                case "mcq-answer": {
+                    newQuestion = new mcqQuestion({
+                        ...common,
+                        options: [...options],
+                    })
+                    break
+                }
+
+                case "checkbox-answer": {
+                    newQuestion = new checkboxQuestion({
+                        ...common,
+                        options: [...options],
+                    })
+                    break
+                }
+
+                case "dropdown-answer": {
+                    newQuestion = new dropdownQuestion({
+                        ...common,
+                        options: [...options],
+                    })
+                    break
+                }
+
+                case "linearscale-answer": {
+                    newQuestion = new linearscaleQuestion({
+                        ...common,
+                        lowRating: lowRating,
+                        highRating: highRating,
+                        lowRatingLabel: lowRatingLabel,
+                        highRatingLabel: highRatingLabel,
+                    })
+                    break
+                }
+
+                case "multiplechoicegrid-answer": {
+                    newQuestion = new multiplechoicegridQuestion({
+                        ...common,
+                        rowLabel: rowLabel,
+                        colLabel: colLabel,
+                    })
+                    break
+                }
+
+                case "checkboxgrid-answer": {
+                    newQuestion = new checkboxgridQuestion({
+                        ...common,
+                        rowLabel: rowLabel,
+                        colLabel: colLabel,
+                    })
+                    break
+                }
+
+                case "date-answer": {
+                    newQuestion = new dateQuestion({ ...common })
+                    break
+                }
+
+                case "time-answer": {
+                    newQuestion = new timeQuestion({ ...common })
+                    break
+                }
+
+                default:
+                    newQuestion = new Question({ ...common })
             }
 
-            case "email-answer": {
-                newQuestion = new emailQuestion({ ...common })
-                break
-            }
+            await newQuestion.save()
+            console.log("Questoin saved!!")
 
-            case "paragraph-answer": {
-                newQuestion = new paragraphQuestion({ ...common })
-                break
-            }
-
-            case "mcq-answer": {
-                newQuestion = new mcqQuestion({
-                    ...common,
-                    options: [...options],
-                })
-                break
-            }
-
-            case "checkbox-answer": {
-                newQuestion = new checkboxQuestion({
-                    ...common,
-                    options: [...options],
-                })
-                break
-            }
-
-            case "dropdown-answer": {
-                newQuestion = new dropdownQuestion({
-                    ...common,
-                    options: [...options],
-                })
-                break
-            }
-
-            case "linearscale-answer": {
-                newQuestion = new linearscaleQuestion({
-                    ...common,
-                    lowRating: lowRating,
-                    highRating: highRating,
-                    lowRatingLabel: lowRatingLabel,
-                    highRatingLabel: highRatingLabel,
-                })
-                break
-            }
-
-            case "multiplechoicegrid-answer": {
-                newQuestion = new multiplechoicegridQuestion({
-                    ...common,
-                    rowLabel: rowLabel,
-                    colLabel: colLabel,
-                })
-                break
-            }
-
-            case "checkboxgrid-answer": {
-                newQuestion = new checkboxgridQuestion({
-                    ...common,
-                    rowLabel: rowLabel,
-                    colLabel: colLabel,
-                })
-                break
-            }
-
-            case "date-answer": {
-                newQuestion = new dateQuestion({ ...common })
-                break
-            }
-
-            case "time-answer": {
-                newQuestion = new timeQuestion({ ...common })
-                break
-            }
-
-            default:
-                newQuestion = new Question({ ...common })
+            form.questions.push(newQuestion)
+            await form.save()
+            console.log("Form saved!!")
+            return res.json(newQuestion)
         }
-
-        await newQuestion.save()
-        console.log("Questoin saved!!")
-
-        form.questions.push(newQuestion)
-        await form.save()
-        console.log("Form saved!!")
-        return res.json(newQuestion)
     } catch (error) {
         return res.send({ success: false, msg: error })
     }
@@ -187,123 +194,131 @@ export async function getQuestionsByFormid(req: Request, res: Response) {
 
 export async function updateQuestion(req: Request, res: Response) {
     try {
-        let moddedBody = { ...req.body }
-        moddedBody["question-type"] = req.body["question-type"]
-        let updatedQuestion
-        // Update by finding Question, so that question type can be changed
-        updatedQuestion = await Question.findOneAndUpdate(
-            { _id: req.body._id },
-            {
-                "question-type": req.body["question-type"],
-            },
-            { new: true }
-        )
-        console.log("First update", updatedQuestion)
-        // Update by finding exact question type, else options won't get updated
-        switch (moddedBody["question-type"]) {
-            case "short-answer":
-                updatedQuestion = await shortQuestion.findOneAndUpdate(
-                    { _id: req.body._id },
-                    {
-                        ...moddedBody,
-                    },
-                    { new: true }
-                )
-                break
-            case "paragraph-answer":
-                updatedQuestion = await paragraphQuestion.findOneAndUpdate(
-                    { _id: req.body._id },
-                    {
-                        ...moddedBody,
-                    },
-                    { new: true }
-                )
-                break
-            case "email-answer":
-                updatedQuestion = await emailQuestion.findOneAndUpdate(
-                    { _id: req.body._id },
-                    {
-                        ...moddedBody,
-                    },
-                    { new: true }
-                )
-                break
-            case "mcq-answer":
-                updatedQuestion = await mcqQuestion.findOneAndUpdate(
-                    { _id: req.body._id },
-                    {
-                        ...moddedBody,
-                    },
-                    { new: true }
-                )
-                break
-            case "checkbox-answer":
-                updatedQuestion = await checkboxQuestion.findOneAndUpdate(
-                    { _id: req.body._id },
-                    {
-                        ...moddedBody,
-                    },
-                    { new: true }
-                )
-                break
-            case "dropdown-answer":
-                updatedQuestion = await dropdownQuestion.findOneAndUpdate(
-                    { _id: req.body._id },
-                    {
-                        ...moddedBody,
-                    },
-                    { new: true }
-                )
-                break
-            case "linearscale-answer":
-                updatedQuestion = await linearscaleQuestion.findOneAndUpdate(
-                    { _id: req.body._id },
-                    {
-                        ...moddedBody,
-                    },
-                    { new: true }
-                )
-                break
-            case "multiplechoicegrid-answer":
-                updatedQuestion = await multiplechoicegridQuestion.findOneAndUpdate(
-                    { _id: req.body._id },
-                    {
-                        ...moddedBody,
-                    },
-                    { new: true }
-                )
-                break
-            case "checkboxgrid-answer":
-                updatedQuestion = await checkboxgridQuestion.findOneAndUpdate(
-                    { _id: req.body._id },
-                    {
-                        ...moddedBody,
-                    },
-                    { new: true }
-                )
-                break
-            case "date-answer":
-                updatedQuestion = await dateQuestion.findOneAndUpdate(
-                    { _id: req.body._id },
-                    {
-                        ...moddedBody,
-                    },
-                    { new: true }
-                )
-                break
-            case "time-answer":
-                updatedQuestion = await timeQuestion.findOneAndUpdate(
-                    { _id: req.body._id },
-                    {
-                        ...moddedBody,
-                    },
-                    { new: true }
-                )
-                break
-            default:
-                updatedQuestion = { data: { msg: "Something went wrong" } }
+        let {formid}=req.body
+        let form: any
+        form = await Form.findById(formid)
+        if (form.isTemplate) {
+            console.log("Template cant be editted")
+            return res.send({ success: false, msg: "Template cant be editted" })
+        } else {
+            let moddedBody = { ...req.body }
+            moddedBody["question-type"] = req.body["question-type"]
+            let updatedQuestion
+            // Update by finding Question, so that question type can be changed
+            updatedQuestion = await Question.findOneAndUpdate(
+                { _id: req.body._id },
+                {
+                    "question-type": req.body["question-type"],
+                },
+                { new: true }
+            )
+            console.log("First update", updatedQuestion)
+            // Update by finding exact question type, else options won't get updated
+            switch (moddedBody["question-type"]) {
+                case "short-answer":
+                    updatedQuestion = await shortQuestion.findOneAndUpdate(
+                        { _id: req.body._id },
+                        {
+                            ...moddedBody,
+                        },
+                        { new: true }
+                    )
+                    break
+                case "paragraph-answer":
+                    updatedQuestion = await paragraphQuestion.findOneAndUpdate(
+                        { _id: req.body._id },
+                        {
+                            ...moddedBody,
+                        },
+                        { new: true }
+                    )
+                    break
+                case "email-answer":
+                    updatedQuestion = await emailQuestion.findOneAndUpdate(
+                        { _id: req.body._id },
+                        {
+                            ...moddedBody,
+                        },
+                        { new: true }
+                    )
+                    break
+                case "mcq-answer":
+                    updatedQuestion = await mcqQuestion.findOneAndUpdate(
+                        { _id: req.body._id },
+                        {
+                            ...moddedBody,
+                        },
+                        { new: true }
+                    )
+                    break
+                case "checkbox-answer":
+                    updatedQuestion = await checkboxQuestion.findOneAndUpdate(
+                        { _id: req.body._id },
+                        {
+                            ...moddedBody,
+                        },
+                        { new: true }
+                    )
+                    break
+                case "dropdown-answer":
+                    updatedQuestion = await dropdownQuestion.findOneAndUpdate(
+                        { _id: req.body._id },
+                        {
+                            ...moddedBody,
+                        },
+                        { new: true }
+                    )
+                    break
+                case "linearscale-answer":
+                    updatedQuestion = await linearscaleQuestion.findOneAndUpdate(
+                        { _id: req.body._id },
+                        {
+                            ...moddedBody,
+                        },
+                        { new: true }
+                    )
+                    break
+                case "multiplechoicegrid-answer":
+                    updatedQuestion = await multiplechoicegridQuestion.findOneAndUpdate(
+                        { _id: req.body._id },
+                        {
+                            ...moddedBody,
+                        },
+                        { new: true }
+                    )
+                    break
+                case "checkboxgrid-answer":
+                    updatedQuestion = await checkboxgridQuestion.findOneAndUpdate(
+                        { _id: req.body._id },
+                        {
+                            ...moddedBody,
+                        },
+                        { new: true }
+                    )
+                    break
+                case "date-answer":
+                    updatedQuestion = await dateQuestion.findOneAndUpdate(
+                        { _id: req.body._id },
+                        {
+                            ...moddedBody,
+                        },
+                        { new: true }
+                    )
+                    break
+                case "time-answer":
+                    updatedQuestion = await timeQuestion.findOneAndUpdate(
+                        { _id: req.body._id },
+                        {
+                            ...moddedBody,
+                        },
+                        { new: true }
+                    )
+                    break
+                default:
+                    updatedQuestion = { data: { msg: "Something went wrong" } }
+            }
+            return res.send(updatedQuestion)
         }
-        return res.send(updatedQuestion)
     } catch (error) {
         return res.send({ success: false, data: error })
     }
